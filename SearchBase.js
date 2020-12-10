59f925d7c8aa69a34726571d01ef9d7fd16ea428
electrumdeception <59322785+electrumdeception@users.noreply.github.com>
2020-12-09 19:53:11 -0500

    Add files via upload

--- SearchBase.js
+++ SearchBase.js
@@ -1,7 +1,6 @@
 // JavaScript Document
 var http = require('http');
 var url = require('url');
-var jaknum = 0;
 const MongoClient = require('mongodb').MongoClient;
 const mongoUrl = "mongodb+srv://pharrington:potatopotato@cluster0.tjx7u.mongodb.net/?retryWrites=true&w=majority";
 client =new MongoClient(mongoUrl,{ useUnifiedTopology: true });
@@ -15,8 +14,7 @@ var cOrT = qobj.cOrT;
 var result = "";
 	res.write("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
 	res.write("<meta charset=\"utf-8\"><title>Company Sort</title>");
-//	"<link href=\"StockStyle.css\" type=\"text/css\" rel=\"stylesheet\">");
-    res.write("</head><body><h1>Results<h1>")
+    res.write("<style>body {background-color:#E27576;text-align: center;font-family: sans-serif;}</style></head><body><h1>Results<h1>")
 	//res.write("the value is: " + searchTerm +  " and " + cOrT);
 	 MongoClient.connect(mongoUrl,{ useUnifiedTopology: true }, function(err, db) {
 		//res.write("<h3>MicCHeck</h3>")
@@ -26,9 +24,9 @@ var result = "";
 		}
 		var dbo = db.db("HW11");
 		var coll = dbo.collection('companies');
-		jaknum+=1;
+
 		if (cOrT== "comp"){
-			res.wriste("<p>You wanted the Ticker that matches company: " + searchTerm +"</p>");
+			res.write("<p>You wanted the Ticker that matches company: " + searchTerm +"</p>");
     		coll.find({company:searchTerm}).toArray(function(err, items){
 				if (err) {
 					console.log("Error: " + err);
@@ -59,9 +57,11 @@ var result = "";
 					console.log("Error: " + err);
 		  		} 
 				else if (items.length == 0){
+					console.log("no resultos");
 					res.write("<p>Unfortuantely we have no results in our database</p>");
 				}
 				else {
+					console.log("some resultos");
 					res.write("<p>" +items[0].company + " has the ticker " + items[0].ticker +"</p>");
 					if (items.length >1){
 					for (var i=1; i<items.length; i++){
